ba7b1cee1607a44db679ec2198b4633f408dd762
proIT324 <pro.it324@gmail.com>
2019-10-08 02:42:22 +0800

    Added sign-up component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.scss';
 
@@ -12,8 +13,9 @@ class SignInAndSignUpPage extends Component {
 	}
 	render() {
 		return (
-			<div className="sign-in-and-sign-out">
+			<div className="sign-in-and-sign-up">
 				<SignIn />
+				<SignUp />
 			</div>
 		);
 	}
