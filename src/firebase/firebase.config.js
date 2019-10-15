67332700cd1cf59ae113c1568fddd85b3127e31e
proIT324 <pro.it324@gmail.com>
2019-10-15 12:33:16 +0800

    Removed firebase config from .gitignore

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBTODN9zO8Vj227fwTPMccCphR9Nym3Wao',
+	authDomain: 'crown-clothing-e3a8c.firebaseapp.com',
+	databaseURL: 'https://crown-clothing-e3a8c.firebaseio.com',
+	projectId: 'crown-clothing-e3a8c',
+	storageBucket: '',
+	messagingSenderId: '703610164653',
+	appId: '1:703610164653:web:475bb7d76fcfa80aaa7d24',
+	measurementId: 'G-Z4S5B4F4W8'
+};
+
+export default firebaseConfig;
