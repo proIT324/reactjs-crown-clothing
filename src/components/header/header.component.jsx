8269b54a458ce51900d3b3a9d52e75312222ced7
proIT324 <pro.it324@gmail.com>
2019-10-15 20:45:26 +0800

    Applied styled component approach to custom button, header, and homepage components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -5,46 +5,46 @@ import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
 
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropDown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import './header.styles.scss';
+import {
+	HeaderContainer,
+	LogoContainer,
+	OptionsContainer,
+	OptionLink
+} from './header.styles';
 
 const Header = ({ currentUser, hidden }) => {
 	return (
-		<div className="header">
-			<Link className="logo-container" to="/">
+		<HeaderContainer>
+			<LogoContainer to="/">
 				<Logo className="logo" />
-			</Link>
-			<div className="options">
-				<Link className="option" to="/shop">
-					SHOP
-				</Link>
-				<Link className="option" to="/contact">
-					CONTACT
-				</Link>
+			</LogoContainer>
+			<OptionsContainer>
+				<OptionLink to="/shop">SHOP</OptionLink>
+				<OptionLink to="/contact">CONTACT</OptionLink>
 				{currentUser ? (
-					<div
-						className="option"
+					<OptionLink
+						as="div"
 						onClick={() => {
 							auth.signOut();
 						}}
 					>
 						SIGN OUT
-					</div>
+					</OptionLink>
 				) : (
-					<Link className="option" to="/signin">
-						SIGN IN
-					</Link>
+					<OptionLink to="/signin">SIGN IN</OptionLink>
 				)}
 				<CartIcon />
-			</div>
+			</OptionsContainer>
 			{hidden ? null : <CartDropDown />}
-		</div>
+		</HeaderContainer>
 	);
 };
 
