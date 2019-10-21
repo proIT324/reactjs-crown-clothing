f8ddb7ec676db30e44a316424c5212f7de7bd8cd
proIT324 <pro.it324@gmail.com>
2019-10-22 07:54:45 +0800

    Created root saga

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,7 @@ import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
-
-import { fetchCollectionsStart } from './shop/shop.sagas';
+import rootSaga from './root-saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -17,7 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
